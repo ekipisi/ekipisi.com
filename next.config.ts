0e6ac1e092725e72feaf4eb349f8bead3120245e
Mustafa Genc <mustafa.genc@enkasystems.com>
2025-04-29 09:28:24 +0300

    nextjs 15.3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  bundlePagesRouterDependencies: true,
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
